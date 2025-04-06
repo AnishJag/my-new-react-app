d6d2656a51fb2f346fe77db58ec725abbd7e83ef
Anish Jagannathan <a.jagannathan@wustl.edu>
2025-04-06 12:25:30 -0500

    HelloWorld commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <h2>Anish Jagannathan - ToDo App</h2>
+    </>
+  );
+}
+
+export default App;
